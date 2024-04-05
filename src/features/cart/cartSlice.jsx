30ec908cadbe323d40f0775945c627207a28c485
Ankit <ankitthakur4965@gmail.com>
2024-04-05 14:10:36 +0530

    img up

--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -1,10 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import tBagFirst from "../../assets/tBagFirst.webp";
-// import tBagSecond from "../../assets/tBagSecond.webp";
-// import tBagThird from "../../assets/tBagThird.webp";
-// import tBagFourth from "../../assets/tBagFourth.webp";
-// import tBagFifth from "../../assets/tBagFifth.webp";
-// import tBagSixth from "../../assets/tBagSixth.webp";
+import tBagFirst from "/tbagFirst.webp";
+import tBagSecond from "/tBagSecond.webp";
+import tBagThird from "/tBagThird.webp";
+import tBagFourth from "/tBagFourth.webp";
+import tBagFifth from "/tBagFifth.webp";
+import tBagSixth from "/tBagSixth.webp";
 
 
 const initialState = {
@@ -12,48 +12,48 @@ const initialState = {
         {
           name: "FORNAX",
           price: 8100,
-          image: "../../assets/tBagFirst.webp",
+          image: tBagFirst,
           oldPrice: 10800,
           _id: 1,
         },
         {
           name: "PLAYBLUE",
-          image: "../../assets/tBagSecond.webp",
+          image: tBagSecond,
           price: 7100,
           oldPrice: 9800,
           _id: 2,
         },
         {
           name: "AMERICAN",
-          image: "../../assets/tBagThird.webp",
+          image: tBagThird,
           price: 7500,
           oldPrice: 9000,
           _id: 3,
         },
         {
           name: "DELL",
-          image: "../../assets/tBagFourth.webp",
+          image: tBagFourth,
           price: 8100,
           oldPrice: 10800,
           _id: 4,
         },
         {
           name: "HPBAG",
-          image: "../../assets/tBagFifth.webp",
+          image: tBagFifth,
           price: 6100,
           oldPrice: 4800,
           _id: 5,
         },
         {
           name: "SKYBAG",
-          image: "../../assets/tBagSixth.webp",
+          image: tBagSixth,
           price: 4500,
           oldPrice: 5900,
           _id: 6,
         },
         {
           name: "AMERICAN",
-          image: "../../assets/tBagThird.webp",
+          image: tBagThird,
           price: 7500,
           oldPrice: 9000,
           _id: 7,
