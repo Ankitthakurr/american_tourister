556abc0a2c6002eef78da55a909594ac8799442a
Ankit <ankitthakur4965@gmail.com>
2024-04-05 10:39:56 +0530

    space

--- src/App.jsx
+++ src/App.jsx
@@ -45,6 +45,7 @@ const App = () => {
                 <ImageContainer />
                 <GradientThird />
                 <FooterOne />
+                
               </>
             }
           />
